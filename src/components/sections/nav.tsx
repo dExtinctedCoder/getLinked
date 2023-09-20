13e434f0f0e8eca2a9e39cfd3bc4d5fe920f6c05
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-20 03:45:10 +0100

    feat: build responsive landing page nav bar UI component

---
+++ src/components/sections/nav.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../utilities/button";
+import LOGO from "../../assets/images/getlinked.png";
+import MENU from "../../assets/icons/Vector (3).svg";
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = (): React.ReactElement => {
+	return (
+		<header className="flex py-7 px-12 items-center justify-between border-b border-b-[#ffffff2d] lg:py-16 lg:px-32">
+			<Link to="/" className="block w-[72px] md:w-[171px]">
+				<img src={LOGO} alt="get-linked.png" />
+			</Link>
+			<div className="cursor-pointer md:hidden">
+				<img src={MENU} alt="menu button" />
+			</div>
+			<nav className="hidden items-center justify-between gap-x-9 md:flex lg:gap-x-14">
+				<NavLink to="/#timeline" className="font-normal text-base text-white">
+					Timeline
+				</NavLink>
+				<NavLink to="/#overview" className="font-normal text-base text-white">
+					Overview
+				</NavLink>
+				<NavLink to="/#faqs" className="font-normal text-base text-white">
+					FAQs
+				</NavLink>
+				<NavLink to="/contact" className="font-normal text-base text-white">
+					Contact
+				</NavLink>
+			</nav>
+			<Button to="/register" text="register" extras="hidden md:flex" />
+		</header>
+	);
+};
+
+export default Nav;
