6ae69fc473c7d4d243f4b4cc2f00fe3f11e09822
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-20 03:49:25 +0100

    feat: add nav bar component to landing page hero section jsx

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "../components/sections/nav";
 
 const HomePage = (): React.ReactElement => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			<section aria-label="hero section">
+				<Nav />
+			</section>
+		</div>
+	);
 };
 
 export default HomePage;
