59dbce77b7da67ac3cb1ad221044f7ba6ab028f2
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-20 13:10:11 +0100

    feat: change media breakpoint for button size

--- src/components/utilities/button.tsx
+++ src/components/utilities/button.tsx
@@ -10,7 +10,7 @@ const Button = ({ text, to, extras }: buttonProps): React.ReactElement => {
 	return (
 		<Link
 			to={to}
-			className={`${extras} flex items-center justify-center pry-color text-white rounded capitalize text-sm py-[14px] px-[46px] md:text-base md:py-4 md:px-[52px]`}
+			className={`${extras} flex items-center justify-center pry-color text-white rounded capitalize text-sm py-[14px] px-[46px] lg:text-base lg:py-4 lg:px-[52px]`}
 		>
 			{text}
 		</Link>
