b7a5a51d776c3ced76b8ef0144449fbbbddeaa34
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-21 23:52:00 +0100

    feat: implement logic to toggle menu bar visibility

--- src/components/sections/nav.tsx
+++ src/components/sections/nav.tsx
@@ -3,14 +3,23 @@ import Button from "../utilities/button";
 import LOGO from "../../assets/images/getlinked.png";
 import MENU from "../../assets/icons/Vector (3).svg";
 import { Link, NavLink } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { OPEN_MENU, toggleMenu } from "../../store/app/app.actions";
+import Menu from "./menu";
 
 const Nav = (): React.ReactElement => {
+	const menuState = useAppSelector((state) => state.appReducer.menuState);
+	const dispatchMenuAction = useAppDispatch();
+
 	return (
-		<header className="flex py-7 px-6 items-center justify-between border-b border-b-[#ffffff2d] lg:py-[31.5px] lg:px-16">
+		<header className="relative flex py-7 px-6 items-center justify-between border-b border-b-[#ffffff2d] lg:py-[31.5px] lg:px-16">
 			<Link to="/" className="block w-[72px] lg:w-[171px]">
 				<img src={LOGO} alt="get-linked.png" />
 			</Link>
-			<div className="cursor-pointer md:hidden">
+			<div
+				onClick={() => dispatchMenuAction(toggleMenu(OPEN_MENU))}
+				className="cursor-pointer md:hidden"
+			>
 				<img src={MENU} alt="menu button" />
 			</div>
 			<nav className="hidden items-center justify-between gap-x-9 md:flex lg:gap-x-14">
@@ -28,6 +37,7 @@ const Nav = (): React.ReactElement => {
 				</NavLink>
 			</nav>
 			<Button to="/register" text="register" extras="hidden md:flex" />
+			{menuState ? <Menu /> : ""}
 		</header>
 	);
 };
