dc391d18a7a8a89116f9f7f2db5b588eefbdb6ea
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-22 00:15:21 +0100

    fix: change menu button icon to svg component

--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../utilities/button";
-import CLOSE from "../../assets/icons/Vector4.svg";
 import { useAppDispatch } from "../../store/hooks";
 import { CLOSE_MENU, toggleMenu } from "../../store/app/app.actions";
+import Close from "../utilities/closeBtn";
 
 const Menu = (): React.ReactElement => {
 	const dispatchMenuAction = useAppDispatch();
@@ -12,15 +12,23 @@ const Menu = (): React.ReactElement => {
 		<aside className="absolute top-4 right-4 left-4 z-20 pry-black py-5 px-8 pb-24 rounded-lg pry-black backdrop-blur-[30px] border-[0.5px] border-[#ffffffa] md:hidden">
 			<div
 				onClick={() => dispatchMenuAction(toggleMenu(CLOSE_MENU))}
-				className="rounded-full w-fit m-auto mr-0 p-[3px] border border-[#903AFF]"
+				className="rounded-full w-fit m-auto mr-0 p-[3px]"
 			>
-				<img className="cursor-pointer" src={CLOSE} alt="close-menu-button" />
+				<Close />
 			</div>
 			<nav className="text-white pt-10 mb-7 flex flex-col gap-y-2 text-lg font-medium font-[Inter]">
-				<Link to="/timeline">Timeline</Link>
-				<Link to="/overview">Overview</Link>
-				<Link to="/faqs">FAQs</Link>
-				<Link to="/contact">Contact</Link>
+				<Link className="w-fit" to="/timeline">
+					Timeline
+				</Link>
+				<Link className="w-fit" to="/overview">
+					Overview
+				</Link>
+				<Link className="w-fit" to="/faqs">
+					FAQs
+				</Link>
+				<Link className="w-fit" to="/contact">
+					Contact
+				</Link>
 			</nav>
 			<Button text="register" to="/register" extras="w-fit" />
 		</aside>
