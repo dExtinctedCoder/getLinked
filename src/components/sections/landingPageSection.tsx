180adc1cae113eba9502cc095d0d80df46029bd9
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-22 04:51:51 +0100

    feat: build re-useable component for landing page sections

---
+++ src/components/sections/landingPageSection.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface sectionProps {
+	label: string;
+	textBox: React.ReactElement;
+	src: string;
+	alt?: string;
+	extras?: string;
+	misc?: React.ReactElement;
+}
+
+const Section = ({
+	textBox,
+	label,
+	extras,
+	src,
+	alt,
+	misc,
+}: sectionProps): React.ReactElement => {
+	return (
+		<section
+			aria-label={label}
+			className={`relative lg:overflow-auto overflow-hidden py-8 px-[42px] flex flex-col items-center gap-y-4 pb-8 lg:py-10 lg:px-16 lg:flex-row lg:gap-x-24 ${extras}`}
+		>
+			<img
+				src={src}
+				alt={alt !== "" ? alt : "alt text"}
+				className="z-10 lg:w-1/2"
+			/>
+			{textBox}
+			{misc}
+		</section>
+	);
+};
+
+export default Section;
