b9e3fb2044b6fb921862466505f353c3149a51b6
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-23 18:52:51 +0100

    style: implement styling based on current page route

--- src/components/utilities/button.tsx
+++ src/components/utilities/button.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 interface buttonProps {
 	text: string;
 	extras?: string;
-	to: string;
+	to?: string;
+	isNavBtn?: boolean;
 }
 
-const Button = ({ text, to, extras }: buttonProps): React.ReactElement => {
+const Button = ({
+	text,
+	to,
+	extras,
+	isNavBtn,
+}: buttonProps): React.ReactElement => {
 	return (
 		<Link
-			to={to}
+			to={to ?? ""}
+			style={({ isActive }) => {
+				return {
+					background:
+						isActive && (isNavBtn ?? false)
+							? "linear-gradient(#150e28, #150e28) padding-box, linear-gradient(270deg, #903aff 0%, #fe34b9 100%) border-box"
+							: "",
+					border:
+						isActive && (isNavBtn ?? false) ? "2px solid transparent" : "",
+				};
+			}}
 			className={`${extras} flex items-center justify-center pry-color text-white rounded capitalize text-sm py-[14px] px-[46px] lg:text-base lg:py-4 lg:px-[52px]`}
 		>
 			{text}
