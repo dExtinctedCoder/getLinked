e0da183c2279a4aa1515208db09c285005499702
dExtinctedCoder <jamiumuhammed011@gmail.com>
2023-09-24 01:13:42 +0100

    feat: implement contact form functionality

--- src/pages/contactPage.tsx
+++ src/pages/contactPage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import * as Yup from "yup";
 import Nav from "../components/sections/nav";
 import BackBtn from "../components/utilities/backButton";
-import Button from "../components/utilities/button";
+import { FormButton } from "../components/utilities/button";
 import INSTAGRAM from "../assets/icons/mdi_instagram.svg";
 import TWITTER_X from "../assets/icons/Vector2.svg";
 import FACEBOOK from "../assets/icons/Vector1.svg";
@@ -12,8 +13,63 @@ import STARGray from "../assets/images/stargray.png";
 import BlurPurple1 from "../assets/images/purple-lens-flare-png.png";
 import BlurPurple2 from "../assets/images/purple-lens-flare-png-2.png";
 import { Link } from "react-router-dom";
+import { request } from "../utils/axios.utils";
+import { useMutation } from "react-query";
+import { queryClient } from "../App";
+import { ErrorMessage, Formik, Form } from "formik";
 
 const ContactPage = (): React.ReactElement => {
+	const urlPath = "/hackathon/contact-form"; // this is the url path after the baseurl for the registration endpoint
+
+	// function to reset form after submission
+	const reset = (fn: () => void): void => {
+		fn();
+		// setUserInfo(defaultState);
+	};
+
+	const postUser = (userDetail: object): void => {
+		createUser(userDetail);
+	};
+
+	// react query function to create user
+	const createUserFn = async (details: object): Promise<object> => {
+		return await request({ url: urlPath, method: "post", data: details });
+	};
+
+	// react-query hook that sends post request to create user
+	const { mutate: createUser } = useMutation(createUserFn, {
+		onSuccess: () => {
+			void queryClient.invalidateQueries("register");
+		},
+		onError: (error: Error) => {
+			throw new Error(error.message);
+		},
+	});
+
+	// validation schema for registration form validation
+	const validationSchema = Yup.object({
+		first_name: Yup.string().required("First name is required"),
+		email: Yup.string()
+			.email("Invalid email format")
+			.required("Email is required"),
+		message: Yup.string().required("Message is required").min(10, "Too short"),
+	});
+
+	const initialValues = {
+		first_name: "",
+		email: "",
+		message: "",
+	};
+
+	// function that handles form submission
+	const handleSubmit = (
+		values: typeof initialValues,
+		{ resetForm }: { resetForm: () => void }
+	): void => {
+		postUser({ ...values });
+		reset(resetForm);
+	};
+
 	return (
 		<div className="relative overflow-hidden pry-black text-white min-h-screen pb-16 lg:pb-32">
 			<div className="hidden md:block">
@@ -70,30 +126,51 @@ const ContactPage = (): React.ReactElement => {
 							Questions or need assistance? <br /> Let us know about it
 						</h4>
 					</section>
-					<form className="flex flex-col items-center justify-between gap-y-6">
-						<input
-							type="text"
-							name="first-name-field"
-							id="first-name-field"
-							placeholder="First Name"
-							className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
-						/>
-						<input
-							type="email"
-							name="email-field"
-							id="email-field"
-							placeholder="Mail"
-							className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
-						/>
-						<textarea
-							name="message"
-							id="message-field"
-							placeholder="Message"
-							rows={6}
-							className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
-						></textarea>
-						<Button text="Submit" />
-					</form>
+					<Formik
+						initialValues={initialValues}
+						validationSchema={validationSchema}
+						onSubmit={handleSubmit}
+					>
+						<Form className="flex flex-col items-center justify-between gap-y-6">
+							<input
+								type="text"
+								name="first-name-field"
+								id="first-name-field"
+								placeholder="First Name"
+								className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
+							/>
+							<ErrorMessage
+								name="first_name"
+								component="small"
+								className="text-red-600"
+							/>
+							<input
+								type="email"
+								name="email-field"
+								id="email-field"
+								placeholder="Mail"
+								className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
+							/>
+							<ErrorMessage
+								name="email"
+								component="small"
+								className="text-red-600"
+							/>
+							<textarea
+								name="message"
+								id="message-field"
+								placeholder="Message"
+								rows={6}
+								className="w-full py-3 px-6 rounded border border-white focus:border-[#D434FE] focus:outline-none bg-[#FFFFFF08] shadow-[0px_4px_4px_0px_#0000003f] text-base font-normal text-white placeholder:text-white"
+							></textarea>
+							<ErrorMessage
+								name="message"
+								component="small"
+								className="text-red-600"
+							/>
+							<FormButton text="Submit" />
+						</Form>
+					</Formik>
 					<div className="py-10 md:hidden">
 						<p className="text-[#D434FE] text-center text-xs pb-2">Share on</p>
 						<div className="flex items-center justify-center gap-x-3">
